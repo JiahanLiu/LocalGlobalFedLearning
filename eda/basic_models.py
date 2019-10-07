dcc49409d7b49b43626788969ab75e27c54e3c81
JiahanLiu <jiahan.jim.liu@gmail.com>
2019-10-07 09:49:40 -0500

    Catboost and EDA

---
+++ eda/basic_models.py
@@ -0,0 +1,107 @@
+import os.path
+from os import path
+
+import numpy as np
+
+import pandas as pd
+
+from sklearn.preprocessing import StandardScaler, LabelEncoder
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import f1_score
+
+from catboost import Pool, CatBoostClassifier, cv
+
+import pickle
+
+DATA_PATH = "../data/"
+RESULTS_PATH = "results/"
+
+train_df = pd.read_csv(DATA_PATH + 'train.csv')
+test_df = pd.read_csv(DATA_PATH + 'test.csv')
+
+both_df = pd.concat([train_df, test_df], axis=0).reset_index(drop=True)
+all_features_df = both_df.copy()
+
+all_features_df.pop('subject')
+all_labels = all_features_df.pop('Activity')
+
+def cat_boost(model_file, accuracy_file, features_df, labels):
+    enc = LabelEncoder()
+    label_encoded = enc.fit_transform(labels)
+
+    X_train, X_test, y_train, y_test = train_test_split(features_df, label_encoded, test_size=0.2, random_state=3)
+
+    clf = CatBoostClassifier(random_seed=3, custom_loss='TotalF1', loss_function='MultiClass', od_type='Iter', od_wait=20)
+    if path.exists(model_file):
+        clf = pickle.load(open(model_file, 'rb'))
+    else:
+        clf.fit(X_train, y_train)
+        pickle.dump(clf, open(model_file, 'wb'))
+
+    score = f1_score(y_test, clf.predict(X_test), average='macro')
+    print("Finished" + model_file)
+    
+    with open(accuracy_file, mode='w+') as file: 
+        file.write('F1 on testset:\t{:.4f}\n'.format(score))
+
+    print('F1 on testset:\t{:.4f}\n'.format(score))
+
+def local_catboost():
+    model_file_p1 = RESULTS_PATH + 'catboost_'
+    model_file_p2 = '.catboost'
+    accuracy_file_p1 = RESULTS_PATH + 'CatBoostAccuracy_'
+    accuracy_file_p2 = '.txt'
+
+    for i in range(1, 31):
+        model_file = model_file_p1 + str(i) + model_file_p2
+        accuracy_file = accuracy_file_p1 + str(i) + accuracy_file_p2
+        local_df = both_df.loc[both_df['subject'] == i]
+        
+        local_df.pop('subject')
+        local_labels = local_df.pop('Activity')
+
+        cat_boost(model_file, accuracy_file, local_df, local_labels)
+
+local_catboost()
+
+def group_catboost5():
+    model_file_p1 = RESULTS_PATH + 'catboost_'
+    model_file_p2 = '.catboost'
+    accuracy_file_p1 = RESULTS_PATH + 'CatBoostAccuracy_'
+    accuracy_file_p2 = '.txt'
+
+    for i in range(0, 6):
+        lbound = i*5+1
+        ubound = i*5+5
+        model_file = model_file_p1 + str(lbound) + "_" + str(ubound) + model_file_p2
+        accuracy_file = accuracy_file_p1 + str(lbound) + "_" + str(ubound) + accuracy_file_p2
+        local_df = both_df.loc[((both_df['subject'] >= lbound) & (both_df['subject'] <= ubound))]
+
+        local_df.pop('subject')
+        local_labels = local_df.pop('Activity')
+
+        cat_boost(model_file, accuracy_file, local_df, local_labels)
+
+group_catboost5()
+
+def catboost_all():
+    model_file_p1 = RESULTS_PATH + 'catboost_'
+    model_file_p2 = '.catboost'
+    accuracy_file_p1 = RESULTS_PATH + 'CatBoostAccuracy_'
+    accuracy_file_p2 = '.txt'
+
+    dicts = [
+        {
+            "model_file": model_file_p1 + "all" + model_file_p2,
+            "accuracy_file": accuracy_file_p1 + "all" + accuracy_file_p2,
+            "features": all_features_df,
+            "labels": all_labels
+        }
+    ]
+
+    cat_boost(dicts[0]["model_file"], dicts[0]["accuracy_file"], dicts[0]["features"], dicts[0]["labels"])
+
+
+
+
+
---
+++ eda/pca.py
@@ -0,0 +1,100 @@
+import numpy as np
+
+import pandas as pd
+from collections import Counter
+
+from matplotlib.colors import rgb2hex
+from matplotlib.cm import get_cmap
+import matplotlib.pyplot as plt
+
+from sklearn.preprocessing import StandardScaler, LabelEncoder
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
+from sklearn.decomposition import PCA
+from sklearn.manifold import TSNE
+
+DATA_PATH = "../data/"
+RESULTS_PATH = "results/"
+
+train_df = pd.read_csv(DATA_PATH + 'train.csv')
+test_df = pd.read_csv(DATA_PATH + 'test.csv')
+
+train_df['Data'] = 'Train'
+test_df['Data'] = 'Test'
+both_df = pd.concat([train_df, test_df], axis=0).reset_index(drop=True)
+both_df['subject'] = '#' + both_df['subject'].astype(str)
+
+label = both_df.pop('Activity')
+
+def print_data_shape():
+    print('Shape Train:\t{}'.format(train_df.shape))
+    print('Shape Test:\t{}'.format(test_df.shape))
+    print(both_df.info())
+
+    print(train_df.head())
+
+    print(
+        pd.DataFrame.from_dict(
+        Counter([col.split('-')[0].split('(')[0] for col in both_df.columns]), 
+            orient='index')
+        .rename(columns={0:'count'})
+        .sort_values('count', ascending=False)
+    )
+
+    print()
+
+    print('Null Values in DataFrame: {}\n'.format(both_df.isna().sum().sum()))
+
+def plot_distribution():
+    label_counts = label.value_counts()
+    index = np.arange(len(label_counts))
+    counts = label_counts.values
+
+    plt.figure(
+        figsize=(18, 5),
+        dpi=70)
+    plt.title("Distribution")
+    plt.bar(index, label_counts.values)
+    plt.xticks(index, label_counts.index)
+    plt.show()
+
+def activity_pca():
+    label_counts = label.value_counts()
+
+    tsne_data = both_df.copy()
+    data_data = tsne_data.pop('Data')
+    subject_data = tsne_data.pop('subject')
+
+    scl = StandardScaler()
+    tsne_data = scl.fit_transform(tsne_data)
+
+    pca = PCA(n_components=0.9, random_state=3)
+    tsne_data = pca.fit_transform(tsne_data)
+
+    tsne = TSNE(random_state=3)
+    tsne_transformed = tsne.fit_transform(tsne_data)
+
+    fig, axarr = plt.subplots(2, 1, figsize=(15,10))
+    n = label.unique().shape[0]
+    colormap = get_cmap('viridis')
+    colors = [rgb2hex(colormap(col)) for col in np.arange(0, 1.01, 1/(n-1))]
+
+    for i, group in enumerate(label_counts.index):
+        # Mask to separate sets
+        mask = (label==group).values
+        axarr[0].scatter(x=tsne_transformed[mask][:,0], y=tsne_transformed[mask][:,1], c=colors[i], alpha=0.5, label=group)
+        axarr[0].set_title('TSNE: Activity Visualisation')
+        axarr[0].legend()
+
+    n = subject_data.unique().shape[0]
+    colormap = get_cmap('gist_ncar')
+    colors = [rgb2hex(colormap(col)) for col in np.arange(0, 1.01, 1/(n-1))]
+
+    for i, group in enumerate(subject_data.unique()):
+        # Mask to separate sets
+        mask = (subject_data==group).values
+        axarr[1].scatter(x=tsne_transformed[mask][:,0], y=tsne_transformed[mask][:,1], c=colors[i], alpha=0.5, label=group)
+
+    axarr[1].set_title('TSNE: Participant Visualisation')
+    plt.savefig(RESULTS_PATH + 'ActivitySeperable.pdf')
+    plt.show()
