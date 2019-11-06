e34bc69209e3ded398d739cda497e9e849332e22
Jiahan Liu <jiahan.jim.liu@gmail.com>
2019-11-06 16:02:20 +0000

    accidently changed client on server2

--- FedMnist/client/federated_client.py
+++ FedMnist/client/federated_client.py
@@ -5,23 +5,25 @@ URL_BASE = "http://167.71.247.3:3000/"
 UPLOAD_URL = URL_BASE + "local_upload"
 DOWNLOAD_URL = URL_BASE + "get_global_param"
 
-FILE_DIR = "client_files/"
-UPLOAD_NAME = "test.txt"
+FILE_DIR = "param_files/"
+UPLOAD_NAME = "upload.txt"
 DOWNLOAD_NAME = "download.txt"
 
 pwd_path = os.path.abspath(os.path.dirname(__file__))
 
-def send_file_to_server(file_dir, file_name):
+def upload_file_to_server(file_dir, file_name):
     with open(file_dir+file_name, 'rb') as file:
-        files = {'file':('file.txt', file)}
+        files = {'file':('node1_param.txt', file)}
         r = requests.post(UPLOAD_URL, files=files)
         print(r)
+        print("File Uploaded")
 
 absolute_path = os.path.join(pwd_path, FILE_DIR)
-# send_file_to_server(absolute_path, UPLOAD_NAME)
+upload_file_to_server(absolute_path, UPLOAD_NAME)
 
 def get_file_from_server(file_dir, file_name):
     r = requests.get(DOWNLOAD_URL, allow_redirects=True)
     open(file_dir+file_name, 'wb').write(r.content)
+    print("File Downloaded")
 
-get_file_from_server(absolute_path, DOWNLOAD_NAME)
+# get_file_from_server(absolute_path, DOWNLOAD_NAME)
