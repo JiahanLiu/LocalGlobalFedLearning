331265ebce27101ba4bf4d6e9262550b4fcfbb23
Jiahan Liu <jiahan.jim.liu@gmail.com>
2019-11-07 00:06:51 +0000

    started python script for server

--- FedMnist/server/app.js
+++ FedMnist/server/app.js
@@ -1,6 +1,7 @@
 const express = require('express')
 var multer = require('multer');
 var fs = require('fs'); 
+const spawn = require("child_process").spawn;
 
 const app = express()
 const port = 3000
@@ -8,11 +9,17 @@ const port = 3000
 const FILES_DIR = '/param_files/'
 var upload = multer({ dest: FILES_DIR});
 
+var one_hot_state = 0b000;
+var N_partitions = 3;
+
 app.get('/', (req, res) => res.send('Federated Learning - Server'))
 
 // File input field name is simply 'file'
 app.post('/upload_local_param', upload.single('file'), function(req, res) {
+    var node_n = req.query.node_n;
+    one_hot_state = one_hot_state | (1<<node_n)
     var file_destination = __dirname + '/param_files/' + req.file.originalname;
+    const pythonProcess = spawn('python',['federated_server.py']);
     fs.rename(req.file.path, file_destination, function(err) {
         if (err) {
             console.log(err);
@@ -25,6 +32,11 @@ app.post('/upload_local_param', upload.single('file'), function(req, res) {
         }
     });
 });
+
+app.get('/fed_avg_done', function(req, res) {
+    status = req.query.status
+    console.log(status)
+})
 
 var base_path = __dirname + FILES_DIR;
 app.get('/get_global_param', function(req, res) {
---
+++ FedMnist/server/federated_server.py
@@ -0,0 +1,5 @@
+import sys
+import request
+
+print("FEDERATED_SERVER.py")
+sys.stdout.flush()
