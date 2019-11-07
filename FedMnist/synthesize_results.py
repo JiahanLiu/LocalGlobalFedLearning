7bfa6afe6afefae706191fa343f27d27fac69ee6
Jiahan Liu <jiahan.jim.liu@gmail.com>
2019-11-07 22:05:41 +0000

    refactor synthesize_results away from util

---
+++ FedMnist/synthesize_results.py
@@ -0,0 +1,22 @@
+import matplotlib.pyplot as plt
+
+def print_shapes(test_loader):
+    examples = enumerate(test_loader)
+    batch_idx, (example_data, example_targets) = next(examples)
+    print(example_data.shape) # torch.Size([1000, 1, 28, 28])
+
+def plot_samples(test_loader):
+    examples = enumerate(test_loader)
+    batch_idx, (example_data, example_targets) = next(examples)
+    print(example_data.shape)
+
+    fig = plt.figure()
+    for i in range(6):
+        plt.subplot(2,3,i+1)
+        plt.tight_layout()
+        plt.imshow(example_data[i][0], cmap='gray', interpolation='none')
+        plt.title("Ground Truth: {}".format(example_targets[i]))
+        plt.xticks([])
+        plt.yticks([])
+        plt.show()
+        
--- FedMnist/util.py
+++ FedMnist/util.py
@@ -1,7 +1,6 @@
 # from model import nn_architectures, data_loader
 
 import torch
-import matplotlib.pyplot as plt
 
 import os
 
@@ -13,24 +12,4 @@ def save_model_to_file(model, file_path):
 def load_model_from_file(model, file_path, device):
     checkpoint = torch.load(file_path, map_location=device)
     model.load_state_dict(checkpoint)
-
-def print_shapes(test_loader):
-    examples = enumerate(test_loader)
-    batch_idx, (example_data, example_targets) = next(examples)
-    print(example_data.shape) # torch.Size([1000, 1, 28, 28])
-
-def plot_samples(test_loader):
-    examples = enumerate(test_loader)
-    batch_idx, (example_data, example_targets) = next(examples)
-    print(example_data.shape)
-
-    fig = plt.figure()
-    for i in range(6):
-        plt.subplot(2,3,i+1)
-        plt.tight_layout()
-        plt.imshow(example_data[i][0], cmap='gray', interpolation='none')
-        plt.title("Ground Truth: {}".format(example_targets[i]))
-        plt.xticks([])
-        plt.yticks([])
-        plt.show()
         
