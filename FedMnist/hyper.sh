cd90f64cb3289f28d06d5a4aed6c7d98e0333af9
Jiahan Liu <jiahan.jim.liu@gmail.com>
2019-11-18 18:07:54 +0000

    minor change

--- FedMnist/hyper.sh
+++ FedMnist/hyper.sh
@@ -2,3 +2,18 @@ time (python3 train.py > /dev/null) &
 time (python3 train.py > /dev/null) & 
 time (python3 train.py > /dev/null) & 
 time (python3 train.py > /dev/null) & 
+
+time (python3 train.py > /dev/null) & 
+time (python3 train.py > /dev/null) & 
+time (python3 train.py > /dev/null) & 
+time (python3 train.py > /dev/null) & 
+
+time (python3 train.py > /dev/null) & 
+time (python3 train.py > /dev/null) & 
+time (python3 train.py > /dev/null) & 
+time (python3 train.py > /dev/null) & 
+
+time (python3 train.py > /dev/null) & 
+time (python3 train.py > /dev/null) & 
+time (python3 train.py > /dev/null) & 
+time (python3 train.py > /dev/null) & 
