718efd33492b94623685c4792fd7924550073dce
JIahan Liu <jiahan.jim.liu@gmail.com>
2019-11-23 06:40:09 +0000

    fixed r2c3

--- FedMnist/synth2.sh
+++ FedMnist/synth2.sh
@@ -1,4 +1,4 @@
-python3 synthesize_results.py -p 4 -g 0 -s 0 -l 3 & 
-python3 synthesize_results.py -p 4 -g 1 -s 13 -l 3 & 
-python3 synthesize_results.py -p 4 -g 2 -s 26 -l 3 & 
-python3 synthesize_results.py -p 4 -g 3 -s 38 -l 3 & 
+python3 synthesize_results.py -p 2 -g 0 -s 0 -l 3 & 
+python3 synthesize_results.py -p 2 -g 1 -s 25 -l 3 & 
+python3 synthesize_results.py -p 3 -g 2 -s 0 -l 3 & 
+python3 synthesize_results.py -p 3 -g 3 -s 25 -l 3 & 
